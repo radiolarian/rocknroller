c6fe06ffc2d5f6005266b1320524102104632752
JoshFandel <joshuafandel@berkeley.edu>
2016-03-11 01:20:37 -0800

    Merrier version that uses the Other way (Server thing..)

--- rocknroller.ino
+++ rocknroller.ino
@@ -10,75 +10,59 @@
 //#define ConsoleBaud 115200
 #include <SPI.h>
 #include <WiFi.h>
-#include <WiFiUdp.h>
 
-int status = WL_IDLE_STATUS;
 char ssid[] = "Yipee! 2.0";      // your network SSID (name) 
 char pass[] = "Catherineisthebest";   // your network password
 int keyIndex = 0;                 // your network key Index number (needed only for WEP)
 
-unsigned int localPort = 2390;      // local port to listen on
+int status = WL_IDLE_STATUS;
 
-char packetBuffer[255]; //buffer to hold incoming packet
-char  ReplyBuffer[] = ""; // a string to send back
+WiFiServer server(80);
 
 // The serial connection to the GPS device
 //SoftwareSerial ss(RXPin, TXPin);
-
-WiFiUDP Udp;
 
 // The TinyGPS++ object
 TinyGPSPlus gps;
 
 void setup()
 {
-  //pinMode(13, OUTPUT); TRYING TO TEST WHAT MADE MY BOARD TURN OFF
+ pinMode(13, OUTPUT);
 
-  //GPS
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600); 
+ // while (!Serial) {
+ //   ; // wait for serial port to connect. Needed for Leonardo only
+ // }
+  
+//  // check for the presence of the shield:
+//  if (WiFi.status() == WL_NO_SHIELD) {
+//    Serial.println("WiFi shield not present"); 
+//    // don't continue:
+//    while(true);
+//  } 
+ 
+  // attempt to connect to Wifi network:
+//  while (status != WL_CONNECTED) { 
+//    Serial.print("Attempting to connect to SSID: ");
+//    Serial.println(ssid);
+//    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
+//    status = WiFi.begin(ssid, pass);
+//
+//    // wait 10 seconds for connection:
+//    delay(10000);
+//  } 
+//  server.begin();
+// // you're connected now, so print out the status:
+// printWifiStatus();
+
+
+   //GPS
    Serial.begin(9600);
    Serial1.begin(9600);
 
    Serial.println("Getting lat/long with TinyGPS");
    Serial.println();
-
-  //Initialize serial and wait for port to open:
-  Serial.begin(9600); 
-  
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-
-  // check for the presence of the shield:
-  if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present");
-    // don't continue:
-    while (true);
-  }
-
-  String fv = WiFi.firmwareVersion();
-  if (fv != "1.1.0") {
-    Serial.println("Please upgrade the firmware");
-  }
-
-  // attempt to connect to Wifi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid);
-
-    // wait 10 seconds for connection:
-    delay(10000);
-  }
-
-  Serial.println("Connected to wifi");
-  printWifiStatus();
-
-  Serial.println("\nStarting connection to server...");
-  // if you get a connection, report back via serial:
-  Udp.begin(localPort);
-
-   
 }
 
 void loop()
@@ -99,58 +83,77 @@ void loop()
     //now save lat/long to SD card
   }
 
-  //---------DON'T KNOW IF THIS PART IF NECESSARY----------
- 
-  // // if there's data available, read a packet
-  // int packetSize = Udp.parsePacket();
-  // if (packetSize) {
-  //   Serial.print("Received packet of size ");
-  //   Serial.println(packetSize);
-  //   Serial.print("From ");
-  //   IPAddress remoteIp = Udp.remoteIP();
-  //   Serial.print(remoteIp);
-  //   Serial.print(", port ");
-  //   Serial.println(Udp.remotePort());
+ // listen for incoming clients
+ // WiFiClient client = server.available();
+ // if (client) {
+ //   Serial.println("new client");
+ //   // an http request ends with a blank line
+ //   boolean currentLineIsBlank = true;
+ //   while (client.connected()) {
+ //     if (client.available()) {
+ //       char c = client.read();
+ //       Serial.write(c);
+ //       // if you've gotten to the end of the line (received a newline
+ //       // character) and the line is blank, the http request has ended,
+ //       // so you can send a reply
+ //       if (c == '\n' && currentLineIsBlank) {
+ //         // send a standard http response header
+ //         client.println("HTTP/1.1 200 OK");
+ //         client.println("Content-Type: text/html");
+ //         client.println("Connection: close");  // the connection will be closed after completion of the response
+ //         client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+ //         client.println();
+ //         client.println("<!DOCTYPE HTML>");
+ //         client.println("<html>");
+         // output the value of the GPS longitude and latitude.
 
-  //   // read the packet into packetBufffer
-  //   int len = Udp.read(packetBuffer, 255);
-  //   if (len > 0) {
-  //     packetBuffer[len] = 0;
-  //   }
-  //   Serial.println("Contents:");
-  //   Serial.println(packetBuffer);
-  
-  //-----------THINK WE CAN JUST USE LINES AFTER THIS-------------
+            //POTENTIALLY WHAT WE WANT
+            // client.println(gps.location.lat(), 6);
+            // client.print(",");
+            // client.print(gps.location.lat(), 6);
+ //         client.println("</html>");
+ //          break;
+ //       }
 
+ //       if (c == '\n') {
+ //         // you're starting a new line
+ //         currentLineIsBlank = true;
+ //       } 
+ //       else if (c != '\r') {
+ //         // you've gotten a character on the current line
+ //         currentLineIsBlank = false;
+ //       }
+ //     }
+ //   }
+ //   // give the web browser time to receive the data
+ //   delay(1);
+   
+ //   // close the connection:
+ //   client.stop();
+ //   Serial.println("client disonnected");
+ // }
 
-    // send a reply, to the IP address and port that sent us the packet we received
-    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    Udp.write("%f, %f", gps.location.lat(), gps.location.lng());
-    Udp.endPacket();
-  }
-
-  //TRYING TO TEST WHAT MADE MY BOARD TURN OFF
-  // digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  // delay(4000);              // wait for a second
-  // digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  // delay(1000);              // wait for a second
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(4000);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
 }
 
-void printWifiStatus() {
-// print the SSID of the network you're attached to:
-Serial.print("SSID: ");
-Serial.println(WiFi.SSID());
-
-// print your WiFi shield's IP address:
-IPAddress ip = WiFi.localIP();
-Serial.print("IP Address: ");
-Serial.println(ip);
-
-// print the received signal strength:
-long rssi = WiFi.RSSI();
-Serial.print("signal strength (RSSI):");
-Serial.print(rssi);
-Serial.println(" dBm");
-}
+// void printWifiStatus() {
+// // print the SSID of the network you're attached to:
+// Serial.print("SSID: ");
+// Serial.println(WiFi.SSID());
+//
+// // print your WiFi shield's IP address:
+// IPAddress ip = WiFi.localIP();
+// Serial.print("IP Address: ");
+// Serial.println(ip);
+//
+// // print the received signal strength:
+// long rssi = WiFi.RSSI();
+// Serial.print("signal strength (RSSI):");
+// Serial.print(rssi);
+// Serial.println(" dBm");
+// }
 
 
