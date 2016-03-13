095533b668df2ff8f203e541642cb4e4ee3d47d7
JoshFandel <joshuafandel@berkeley.edu>
2016-03-12 17:40:32 -0800

    adding new Bluefruit LE attampt

--- rocknroller.ino
+++ rocknroller.ino
@@ -1,3 +1,10 @@
+#include <Adafruit_WINC1500.h>
+#include <Adafruit_WINC1500Client.h>
+#include <Adafruit_WINC1500MDNS.h>
+#include <Adafruit_WINC1500Server.h>
+#include <Adafruit_WINC1500SSLClient.h>
+#include <Adafruit_WINC1500Udp.h>
+
 //Serial1 Code inspiration from: https://forums.adafruit.com/viewtopic.php?f=25&t=85437
 //Used Wifi Code from: https://www.arduino.cc/en/Tutorial/WiFiWebServer
 //Other Wifi Code Could be: https://www.arduino.cc/en/Tutorial/WiFiSendReceiveUDPString
@@ -11,16 +18,13 @@
 #include <SPI.h>
 #include <WiFi.h>
 
-char ssid[] = "Yipee! 2.0";      // your network SSID (name) 
-char pass[] = "Catherineisthebest";   // your network password
+char ssid[] = "CalVisitor";      // your network SSID (name) 
+char pass[] = "";   // your network password
 int keyIndex = 0;                 // your network key Index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
 
 WiFiServer server(80);
-
-// The serial connection to the GPS device
-//SoftwareSerial ss(RXPin, TXPin);
 
 // The TinyGPS++ object
 TinyGPSPlus gps;
@@ -35,31 +39,31 @@ void setup()
  //   ; // wait for serial port to connect. Needed for Leonardo only
  // }
   
-//  // check for the presence of the shield:
-//  if (WiFi.status() == WL_NO_SHIELD) {
-//    Serial.println("WiFi shield not present"); 
-//    // don't continue:
-//    while(true);
-//  } 
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present"); 
+    // don't continue:
+    while(true);
+  } 
  
   // attempt to connect to Wifi network:
-//  while (status != WL_CONNECTED) { 
-//    Serial.print("Attempting to connect to SSID: ");
-//    Serial.println(ssid);
-//    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
-//    status = WiFi.begin(ssid, pass);
-//
-//    // wait 10 seconds for connection:
-//    delay(10000);
-//  } 
-//  server.begin();
-// // you're connected now, so print out the status:
-// printWifiStatus();
+  while (status != WL_CONNECTED) { 
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  } 
+  server.begin();
+ // you're connected now, so print out the status:
+ printWifiStatus();
 
 
    //GPS
    Serial.begin(9600);
-   Serial1.begin(9600);
+   Serial1.begin(115200);
 
    Serial.println("Getting lat/long with TinyGPS");
    Serial.println();
@@ -83,55 +87,55 @@ void loop()
     //now save lat/long to SD card
   }
 
- // listen for incoming clients
- // WiFiClient client = server.available();
- // if (client) {
- //   Serial.println("new client");
- //   // an http request ends with a blank line
- //   boolean currentLineIsBlank = true;
- //   while (client.connected()) {
- //     if (client.available()) {
- //       char c = client.read();
- //       Serial.write(c);
- //       // if you've gotten to the end of the line (received a newline
- //       // character) and the line is blank, the http request has ended,
- //       // so you can send a reply
- //       if (c == '\n' && currentLineIsBlank) {
- //         // send a standard http response header
- //         client.println("HTTP/1.1 200 OK");
- //         client.println("Content-Type: text/html");
- //         client.println("Connection: close");  // the connection will be closed after completion of the response
- //         client.println("Refresh: 5");  // refresh the page automatically every 5 sec
- //         client.println();
- //         client.println("<!DOCTYPE HTML>");
- //         client.println("<html>");
-         // output the value of the GPS longitude and latitude.
+//  listen for incoming clients
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+//          output the value of the GPS longitude and latitude.
 
             //POTENTIALLY WHAT WE WANT
-            // client.println(gps.location.lat(), 6);
-            // client.print(",");
-            // client.print(gps.location.lat(), 6);
- //         client.println("</html>");
- //          break;
- //       }
+//             client.println(gps.location.lat(), 6);
+             client.print(",");
+//             client.print(gps.location.lat(), 6);
+          client.println("</html>");
+           break;
+        }
 
- //       if (c == '\n') {
- //         // you're starting a new line
- //         currentLineIsBlank = true;
- //       } 
- //       else if (c != '\r') {
- //         // you've gotten a character on the current line
- //         currentLineIsBlank = false;
- //       }
- //     }
- //   }
- //   // give the web browser time to receive the data
- //   delay(1);
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } 
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
    
- //   // close the connection:
- //   client.stop();
- //   Serial.println("client disonnected");
- // }
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+  }
 
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(4000);              // wait for a second
@@ -139,21 +143,21 @@ void loop()
   delay(1000);              // wait for a second
 }
 
-// void printWifiStatus() {
-// // print the SSID of the network you're attached to:
-// Serial.print("SSID: ");
-// Serial.println(WiFi.SSID());
-//
-// // print your WiFi shield's IP address:
-// IPAddress ip = WiFi.localIP();
-// Serial.print("IP Address: ");
-// Serial.println(ip);
-//
-// // print the received signal strength:
-// long rssi = WiFi.RSSI();
-// Serial.print("signal strength (RSSI):");
-// Serial.print(rssi);
-// Serial.println(" dBm");
-// }
+ void printWifiStatus() {
+ // print the SSID of the network you're attached to:
+ Serial.print("SSID: ");
+ Serial.println(WiFi.SSID());
+
+ // print your WiFi shield's IP address:
+ IPAddress ip = WiFi.localIP();
+ Serial.print("IP Address: ");
+ Serial.println(ip);
+
+ // print the received signal strength:
+ long rssi = WiFi.RSSI();
+ Serial.print("signal strength (RSSI):");
+ Serial.print(rssi);
+ Serial.println(" dBm");
+ }
 
 
